15ceea89701e6a888356b5c0d34d9cbc457691e1
aoki <aokinobu@gmail.com>
2017-01-15 15:34:21 +0900

    version number

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-. build.env
-echo docker-compose -f docker-compose.release.yml up
-docker-compose -f docker-compose.release.yml up
-docker commit glytoucanWurcsToCompositionv1.2.7-TOCO glycoinfo.org:5000/glytoucan_wurcstocomposition:v1.2.7-TOCO
-docker push glycoinfo.org:5000/glytoucan_wurcstocomposition:v1.2.7-TOCO
+. ./build.env
+echo version=$VERSION
+echo docker-compose -f docker-compose.release.yml up --remove-orphans
+docker-compose -f docker-compose.release.yml up --remove-orphans
+docker commit glytoucanWurcsToCompositionv1.2.1-SNAPSHOT glycoinfo.org:5000/glytoucan_wurcstocomposition:v1.2.1-SNAPSHOT
+docker push glycoinfo.org:5000/glytoucan_wurcstocomposition:v1.2.1-SNAPSHOT
