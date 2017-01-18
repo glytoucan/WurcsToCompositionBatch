d9fbbcc7e946fd2a422bed757e0c57f5ea6aa32b
aoki <aokinobu@gmail.com>
2017-01-18 12:53:58 +0900

    update versions and enablebatch

--- src/main/java/org/glycoinfo/batch/wurcs/subsumption/composition/app/WurcsCompositionSparqlBatch.java
+++ src/main/java/org/glycoinfo/batch/wurcs/subsumption/composition/app/WurcsCompositionSparqlBatch.java
@@ -1,14 +1,12 @@
 package org.glycoinfo.batch.wurcs.subsumption.composition.app;
 
-import org.glycoinfo.convert.GlyConvertConfig;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class WurcsCompositionSparqlBatch {
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
